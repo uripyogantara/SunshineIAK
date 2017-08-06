644b26b276460399af202193be73ca10f59058a7
Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2017-08-06 13:42:03 +0800

    Create model based on real API

---
+++ app/src/main/java/com/example/satyakresna/sunshineiak/model/WeatherItem.java
@@ -0,0 +1,64 @@
+package com.example.satyakresna.sunshineiak.model;
+
+import java.util.List;
+
+/**
+ * Created by satyakresna on 06-Aug-17.
+ */
+
+public class WeatherItem {
+    private long dt;
+    private Temp temp;
+    private double pressure;
+    private int humidity;
+    private double speed;
+    private List<Weather> weather;
+
+    public long getDt() {
+        return dt;
+    }
+
+    public void setDt(long dt) {
+        this.dt = dt;
+    }
+
+    public Temp getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Temp temp) {
+        this.temp = temp;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+}
