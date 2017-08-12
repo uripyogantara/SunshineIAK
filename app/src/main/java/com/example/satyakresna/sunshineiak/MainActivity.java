6550b1fa66a67f66b04f2579646d953e970d00da
Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2017-08-12 10:47:47 +0800

    Implement adapter in MainActivity based on real data

--- app/src/main/java/com/example/satyakresna/sunshineiak/MainActivity.java
+++ app/src/main/java/com/example/satyakresna/sunshineiak/MainActivity.java
@@ -4,9 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.satyakresna.sunshineiak.adapter.ForecastAdapter;
+import com.example.satyakresna.sunshineiak.model.DailyForecast;
 import com.example.satyakresna.sunshineiak.model.DummyForecast;
+import com.example.satyakresna.sunshineiak.model.WeatherItem;
+import com.example.satyakresna.sunshineiak.utilities.Constant;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,9 +26,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    private List<DummyForecast> dummyForecastList = new ArrayList<>();
+    public static final String TAG = MainActivity.class.getSimpleName();
+    public List<WeatherItem> weatherItemList = new ArrayList<>();
     @BindView(R.id.rv_forecast) RecyclerView mRecyclerView;
     private ForecastAdapter mAdapter;
+    private Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +38,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-        mAdapter = new ForecastAdapter(dummyForecastList);
+        mAdapter = new ForecastAdapter(weatherItemList);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setAdapter(mAdapter);
 
-        populateData();
+        getDataFromAPI("-8.581930", "115.177059", "10", "metric");
     }
 
-    private void populateData() {
-        for (int i = 0; i <= 10; i++) {
-            DummyForecast dummyForecast = new DummyForecast("Sunday", "Sunny", 23, 18, R.drawable.art_clear);
-            dummyForecastList.add(dummyForecast);
-        }
-        mAdapter.notifyDataSetChanged();
+    private void getDataFromAPI(String lat, String lon, String cnt, String units) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = Constant.URL_API + Constant.PARAM_DAILY +
+                Constant.PARAM_LAT + lat + "&" + Constant.PARAM_LON + lon + "&" +
+                Constant.PARAM_CNT + cnt + "&" + Constant.PARAM_UNIT + units + "&" +
+                Constant.PARAM_API_KEY + Constant.API_KEY;
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            DailyForecast dailyForecast = gson.fromJson(response, DailyForecast.class);
+                            for (WeatherItem item : dailyForecast.getList()) {
+                                weatherItemList.add(item);
+                            }
+                            mAdapter.notifyDataSetChanged();
+                        } catch (Exception e) {
+                            Log.e(TAG, e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error != null) {
+                            Log.e(TAG, error.getMessage());
+                        } else {
+                            Log.e(TAG, "Something error happened!");
+                        }
+                    }
+                }
+        );
+        requestQueue.add(stringRequest);
     }
 }
